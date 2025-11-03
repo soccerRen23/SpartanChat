19b204d4ea74e1c778c680cea130d9d4b722616c
masanori <soccer.rai.25@gmail.com>
2025-11-03 14:01:19 +0900

    static templates 認証機能仮作成

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template,redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+    # return redirect(url_for('login'))
+
+
+@app.route('/signup', methods=['GET'])
+def signup():
+    return render_template('auth/signup.html')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True)
